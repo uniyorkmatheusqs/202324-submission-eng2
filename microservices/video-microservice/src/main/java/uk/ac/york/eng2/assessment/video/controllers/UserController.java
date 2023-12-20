63a8887d6d9300d53b309ba4f2d9d99a94eb9f85
Matheus Quintao <matheus.quintao@dito.com.br>
2023-12-20 23:10:19 +0000

    Create microservices and finish DSL

---
+++ microservices/video-microservice/src/main/java/uk/ac/york/eng2/assessment/video/controllers/UserController.java
@@ -0,0 +1,17 @@
+package uk.ac.york.eng2.assessment.video.controllers;
+
+import java.net.http.HttpResponse;
+
+public class UserController implements IUserController {
+
+    public HttpResponse<String> create(CreateVideoDTO createVideoDTO) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public HttpResponse<String> get(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
